7a2ba95ee2ceb393252f110fb53850b4699876dd
Deivid-Sena-DSC <deivid.sena.dsc@gmail.com>
2025-01-19 18:57:43 -0300

    update

--- forms.py
+++ forms.py
@@ -1,3 +1,18 @@
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class FormCriarConta(Flaskform):
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired(), Length(3, 20)])
+    email_criar_conta = StringField('Email', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao = PasswordField('Confimação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()]) 
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    lembrar_dados = BooleanField('Lembrar dados de acesso')
+    botao_submit_login = SubmitField('Fazer Login')
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
+from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0be83548ee3e85c769b3b51d1324da50'
 
 lista_usuarios = ['Paulo', 'lira', 'Deivid']
 
@@ -17,9 +20,20 @@ def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login_criar_conta.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso! Email: {form_login.email.data}', "alert-success")
+        return redirect(url_for('home'))
+    
+    if form_criarconta.validate_on_submit() and "botao_submit_criarconta" in request.form:    
+        flash(f'Conta criada com sucesso para o email: {form_criarconta.email_criar_conta.data}', 'alert-success')
+        return redirect(url_for('home'))
+    
+    return render_template('login_criar_conta.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
     app.run(debug=True)
